399cdcb6e3fdbf2f1de10d777d1b6bda242ae099
Konstantin Denerz <konstantin.denerz@gmail.com>
2023-06-19 17:01:19 +0200

    update

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatSidenavModule],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss']
 })
