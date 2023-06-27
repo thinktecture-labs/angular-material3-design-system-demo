4a18491281e03169593d3fa5b175136445f40636
Konstantin Denerz <konstantin.denerz@gmail.com>
2023-06-27 20:22:57 +0200

    update

--- src/app/common/navigation/navigation.component.ts
+++ src/app/common/navigation/navigation.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatRippleModule} from '@angular/material/core';
 import {MatListModule} from '@angular/material/list';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'labs-navigation',
   standalone: true,
-  imports: [CommonModule, MatListModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, MatListModule, RouterLink, RouterLinkActive, MatRippleModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
