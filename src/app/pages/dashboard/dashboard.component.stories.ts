a01c7450d43982c9c779f1216e20aef2f96b6a05
Konstantin Denerz <konstantin.denerz@gmail.com>
2023-07-30 13:20:49 +0200

    update

--- src/app/pages/dashboard/dashboard.component.stories.ts
+++ src/app/pages/dashboard/dashboard.component.stories.ts
@@ -4,6 +4,9 @@ import {DashboardComponent} from './dashboard.component';
 export default {
   title: 'Pages / Dashboard',
   component: DashboardComponent,
+  tag: {
+    bubu: 42
+  }
 } as Meta;
 
 const Template: Story<DashboardComponent> = (args: DashboardComponent) => ({
