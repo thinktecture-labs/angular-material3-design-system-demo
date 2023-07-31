bf6231e55d0b6b27f3f1267b2c982e2270fdcb19
Konstantin Denerz <konstantin.denerz@gmail.com>
2023-07-31 11:38:45 +0200

    update

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,21 @@
-import type { Preview } from "@storybook/angular";
-import { setCompodocJson } from "@storybook/addon-docs/angular";
+import {setCompodocJson} from "@storybook/addon-docs/angular";
+import type {Preview} from "@storybook/angular";
 import docJson from "../documentation.json";
+import {viewports} from './viewports';
+
 setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: {argTypesRegex: "^on[A-Z].*"},
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewport: {
+      viewports,
     },
   },
 };
---
+++ .storybook/viewports.ts
@@ -0,0 +1,51 @@
+export const viewports = {
+    xsmall: {
+        name: 'xsmall',
+        styles: {
+            width: '350px',
+            height: '960px',
+        },
+    },
+    small: {
+        name: 'small',
+        styles: {
+            width: '600px',
+            height: '960px',
+        },
+    },
+    medium: {
+        name: 'medium',
+        styles: {
+            width: '1024px',
+            height: '960px',
+        },
+    },
+    large: {
+        name: 'large',
+        styles: {
+            width: '1440px',
+            height: '960px',
+        },
+    },
+    xlarge: {
+        name: 'xlarge',
+        styles: {
+            width: '1920px',
+            height: '960px',
+        },
+    },
+    iphone678: {
+        name: 'iPhone 6/7/8',
+        styles: {
+            width: '375px',
+            height: '667px',
+        },
+    },
+    iphonex: {
+        name: 'iPhone X',
+        styles: {
+            width: '375px',
+            height: '812px',
+        },
+    },
+};
